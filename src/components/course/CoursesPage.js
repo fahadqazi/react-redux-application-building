4359a8ab047b14bc3f11c09025eb6730efc092dc
Fahad Qazi <fahadqazi@gmail.com>
2017-08-11 23:11:52 +0100

    add ability to add courses

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -4,10 +4,10 @@ import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component{
   constructor(props){
-    super(props)
+    super(props);
     this.state = {
       course: {title: ''}
-    }
+    };
     this.onClickSave = this.onClickSave.bind(this);
     this.onTitleChange = this.onTitleChange.bind(this);
   }
@@ -15,17 +15,22 @@ class CoursesPage extends React.Component{
   onTitleChange(event){
     const course = this.state.course;
     course.title = event.target.value;
-    this.setState({course: course})
+    this.setState({course: course});
   }
 
   onClickSave(){
     this.props.dispatch(courseActions.createCourse(this.state.course));
   }
 
+  courseRow(course, index){
+    return <div key={index}>{course.title}</div>;
+  }
+
   render(){
     return(
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add Courses</h2>
         <input
           type="text"
@@ -39,12 +44,18 @@ class CoursesPage extends React.Component{
     );
   }
 }
+
+CoursesPage.propTypes = {
+  dispatch: React.PropTypes.func.isRequired,
+  courses: React.PropTypes.array.isRequired
+};
+
 // ownProps is components own props
 // can be used to access routing related props injected by React Router
 function mapStateToProps(state, ownProps) {
   return {
     courses: state.courses
-  }
+  };
 }
 
 // function mapDispatchToProps(dispatch) {
