08d4221eab4f6bd04311bd77c0584187501312e5
Fahad Qazi <fahadqazi@gmail.com>
2017-08-13 10:04:51 +0100

    set up initial data fetch with thunk

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,11 +1,12 @@
-import * as types from '../actions/actionTypes'
+import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state=[], action){
   switch(action.type) {
-    case types.CREATE_COURSE:
-      return [...state, Object.assign({}, action.course)];
+    case types.LOAD_COURSES_SUCCESS:
+      console.log(action.courses)
+      return action.courses;
 
     default:
-      return state
+      return state;
   }
 }
