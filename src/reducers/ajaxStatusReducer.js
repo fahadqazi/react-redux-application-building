fad39aa4a2e0e2fa2aaed90df0d127c0abe7427a
Fahad Qazi <fahadqazi@gmail.com>
2017-08-29 00:12:44 +0100

    added promise to control page redirect

--- src/reducers/ajaxStatusReducer.js
+++ src/reducers/ajaxStatusReducer.js
@@ -2,7 +2,8 @@ import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
 function actionTypeEndsInSuccess(type) {
-  return type.substring(type.length - 8) === 'success';
+  console.log(type)
+  return type.substring(type.length - 8) == '_SUCCESS';
 }
 
 export default function ajaxStatusReducer(state = initialState.ajaxCallsInProgress, action) {
