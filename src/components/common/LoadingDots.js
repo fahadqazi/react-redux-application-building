32bb99c269c6ae1d30f28b232eaa673b1c811e32
Fahad Qazi <fahadqazi@gmail.com>
2017-08-28 23:11:12 +0100

    adding Loading component

---
+++ src/components/common/LoadingDots.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+
+class LoadingDots extends Component{
+  constructor(props, context){
+    super(props, context);
+
+    this.state = {frame: 1}
+  }
+
+  componentDidMount(){
+    this.interval = setInterval(() => {
+      this.setState({
+        frame: this.state.frame + 1
+      });
+    }, this.props.interval);
+  }
+
+  componentWillMount(){
+    clearInterval(this.interval);
+  }
+
+  render(){
+    let dots = this.state.frame % (this.props.dots + 1);
+    let text = '';
+    while (dots > 0){
+      text += '.';
+      dots--;
+    }
+    return <span {...this.props}>{text}&nbsp;</span>;
+  }
+}
+
+LoadingDots.defaultProps = {
+  interval: 300, dots: 3
+};
+
+LoadingDots.propTypes = {
+  interval: React.PropTypes.number,
+  dots: React.PropTypes.number
+}
+
+export default LoadingDots;
