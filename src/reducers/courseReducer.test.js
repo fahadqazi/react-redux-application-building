c0b29c11593b97fa2a9313fdc33972fb4c5f87f6
Fahad Qazi <fahadqazi@gmail.com>
2017-08-30 23:47:37 +0100

    testing thunks

--- src/reducers/courseReducer.test.js
+++ src/reducers/courseReducer.test.js
@@ -16,6 +16,21 @@ describe('Course Reducer', () => {
   });
 
   it('should update course when passed UPDATE_COURSE_SUCCESS', () => {
+    const initialState = [
+      {id: 'A', title: 'A'},
+      {id: 'B', title: 'B'},
+      {id: 'C', title: 'C'}
+    ];
 
+    const course = {id: 'B', title: 'New Title'}
+    const action = actions.updateCourseSuccess(course);
+
+    const newState = courseReducer(initialState, action);
+    const updatedCourse = newState.find(a => a.id == course.id);
+    const untouchedCourse = newState.find(a => a.id == 'A');
+
+    expect(updatedCourse.title).toEqual('New Title');
+    expect(untouchedCourse.title).toEqual('A');
+    expect(newState.length).toEqual(3);
   });
 });
