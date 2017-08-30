b359b7dbe6c63add073e510a574cd06f3c636331
Fahad Qazi <fahadqazi@gmail.com>
2017-08-30 23:28:48 +0100

    writing tests for actions and reducers

---
+++ src/reducers/courseReducer.test.js
@@ -0,0 +1,21 @@
+import expect from 'expect';
+import courseReducer from './courseReducer';
+import * as actions from '../actions/courseActions';
+
+describe('Course Reducer', () => {
+  it('should add course when passed CREATE_COURSE_SUCCESS', () => {
+    const initialState = [
+      {title: 'A'},
+      {title: 'B'}
+    ];
+    const newCourse = {title: 'C'};
+    const action = actions.createCourseSuccess(newCourse);
+    const newState = courseReducer(initialState, action);
+    expect(newState.length).toEqual(3);
+    expect(newState[0].title).toEqual('A');
+  });
+
+  it('should update course when passed UPDATE_COURSE_SUCCESS', () => {
+
+  });
+});
